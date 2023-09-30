6c1ee8f65f3a808b496f3c427555e21ceafd1890
Esha Fatima <85702449+esha-fatima@users.noreply.github.com>
2023-09-30 10:20:26 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+   distDir: 'build', // Specify your custom build folder name here
+}
 
 module.exports = nextConfig
