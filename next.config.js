6d11239aa318ac0a4dfc5230ead17dcce5ae25d7
Esha Fatima <eshafatima2001@gmail.com>
2023-09-30 10:26:19 +0300

    adding commment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-   distDir: 'build', // Specify your custom build folder name here
+   distDir: 'build', // Specify your custom build folder name here done
 }
 
 module.exports = nextConfig
